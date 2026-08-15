78dff16aaa5819954661211d209d268825b75771
vinayak097 <vinayak20injamure@gmail.com>
2026-08-15 13:57:59 +0530

    socket connection server starting setup

---
+++ apps/server/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Player connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Player disconnected:", socket.id);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
